fddee9f7529291c78fa189d6155b3a6ae9843acd
Sven Baumann <baumann.sv@gmail.com>
2015-08-01 15:04:54 +0200

    register the frontend module

---
+++ contao/config/config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * contao-custom-search
+ *
+ * Copyright (C) contao-custom-search
+ *
+ * @package   contao-custom-search
+ * @file      config.php
+ * @author    Sven Baumann <baumann.sv@gmail.com>
+ * @author    Dominik Tomasi <dominik.tomasi@gmail.com>
+ * @license   LGPL-3.0+
+ * @copyright Copyright 2015 ContaoBlackForest
+ */
+
+$GLOBALS['FE_MOD']['application']['custom-search'] = 'ContaoBlackForest\Module\CustomSearch\Module';
