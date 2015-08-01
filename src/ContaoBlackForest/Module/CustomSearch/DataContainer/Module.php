f8dd24869cadaa249de77b576dd593f5e9a0c21f
Sven Baumann <baumann.sv@gmail.com>
2015-08-01 15:46:37 +0200

    create the module data container class

---
+++ src/ContaoBlackForest/Module/CustomSearch/DataContainer/Module.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * contao-custom-search
+ *
+ * Copyright (C) contao-custom-search
+ *
+ * @package   contao-custom-search
+ * @file      Module.php
+ * @author    Sven Baumann <baumann.sv@gmail.com>
+ * @author    Dominik Tomasi <dominik.tomasi@gmail.com>
+ * @license   LGPL-3.0+
+ * @copyright Copyright 2015 ContaoBlackForest
+ */
+
+
+namespace ContaoBlackForest\Module\CustomSearch\DataContainer;
+
+
+class Module
+{
+
+}
